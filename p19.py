46252e004a2aee9981baac25e47b624cf482df63
Dario Clavijo <clavijodario@gmail.com>
2020-12-03 14:35:57 -0200

    add p19.py

--- p19.py
+++ p19.py
@@ -1,7 +1,18 @@
-from functools import reduce
-from gmpy2 import fac
+from datetime import datetime
+from datetime import timedelta
+import calendar
 
-def p19(n):
-  return reduce(lambda a,b : a+b,list(map(int,list(str(fac(n))))))
+def p19():
+  s = 0
+  start = datetime.strptime("1 Jan 1901","%d %b %Y")
+  end =  datetime.strptime("31 Dec 2000","%d %b %Y")
+  tmp = start
+  while tmp <= end:
+    if tmp.day ==1 and tmp.isoweekday() == 7:
+      print(tmp,tmp.strftime("%A"),calendar.isleap(tmp.year))
+      s+=1
+    tmp += timedelta(days=1)
+  return s
 
-print(p19(100))
+
+print(p19())
